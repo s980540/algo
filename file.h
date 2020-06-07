bbb57b5c2529b60c072442b7774740206ae72f81
s980540 <s980540@ee.yzu.edu.tw>
2020-06-08 01:23:55 +0800

    [2020.06.08] Developed file feaures. Added file_bin_to_c_array().
    file.c: added file_bin_to_c_array(), file_open(), and file_close().
    
    On branch algo_file
    
    Changes to be committed:
    	modified:   file.c
    	modified:   file.h

--- file.h
+++ file.h
@@ -1,9 +1,19 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
+typedef struct _ALGO_FILE
+{
+        FILE *file;
+        char *file_name;
+} ALGO_FILE, *PALGO_FILE;
+
 void read_file_test(void);
+int file_open(ALGO_FILE *fp, const char *name, const char *mode);
+int file_close(ALGO_FILE *fp);
 
 long file_get_bin_size(FILE *fp);
 long file_get_bin_size2(const char *file_name);
+
+int file_bin_to_c_array(const char *bin_name, const char *array_name);
 
 #endif  /* ~ __FILE_H__ */
