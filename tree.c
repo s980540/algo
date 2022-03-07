6dc5b956a1aa1975928a153401556c8356197a70
stevejhang <s980540@ee.yzu.edu.tw>
2022-03-07 11:10:55 +0800

    Add tree algorithms

--- tree.c
+++ tree.c
@@ -60,7 +60,146 @@ ret_code menu_func_tree(int argc, char **argv)
     return ret;
 }
 
+struct _too
+{
+    char a;
+    struct tree tree;
+};
+
+#define ELE_NUM (9)
+#define STACK_SIZE (256)
+
+static struct tree *_stack[STACK_SIZE];
+
+void tree_inorder_traverse_test(struct tree *t)
+{
+    // T1. [Initialize] Set stack A empty, and set the link variable P <- T.
+    int sp = 0;
+    struct tree *p = t;
+
+
+    printf("inorder traverse start >>>\n");
+
+    while (1) {
+    T2:
+        // T2. [P = EMPTY?] If P is empty, go to step T4.
+        if (p == NULL) {
+            // printf("p = NULL\n");
+            goto T4;
+        }
+
+        // T3. [STACK <= P] Set A <= P. Then set P <- LLINK(P) and return to step T2.
+        _stack[sp++] = p;
+
+        // debug
+        // printf("push %c\n", tree_entry(p, struct _too, tree)->a);
+        // printf("LLINK(%c): %c\n", 
+        //     tree_entry(p, struct _too, tree)->a,
+        //     ((p->llink == NULL) ? 'n' : (tree_entry(p->llink, struct _too, tree)->a)));
+
+        // P <- LLINK(P)
+        p = p->llink;
+        goto T2;
+
+    T4:
+        // T4. [P <= STACK] If stack A is empty, the algorithm terminates; otherwise set P <= A.
+        if (sp == 0) {
+            // debug
+            // printf("STACK IS EMPTY\n");
+            break;
+        }
+
+        // debug
+        // printf("sp: %d\n", sp);
+        // printf("_stack(%d): %c\n", sp - 1,
+        //     tree_entry(_stack[sp - 1], struct _too, tree)->a);
+
+        p = _stack[--sp];
+
+        // debug
+        // printf("RLINK(%c): %c\n", 
+        //     tree_entry(p, struct _too, tree)->a,
+        //     ((p->rlink == NULL) ? 'n' : (tree_entry(p->rlink, struct _too, tree)->a)));
+
+        // T5. [Visit P] Visit NODE(P). Then set P <- RLINK(P) and return to step T2
+        printf("%c ", tree_entry(p, struct _too, tree)->a);
+
+        p = p->rlink;
+        goto T2;
+    }
+
+    printf("\n<<< inorder traverse end\n");
+}
+
 void tree_test(void)
 {
+    u32 i;
+
     printf("Hello tree algorithm!\n");
+
+    // TREE(too_root);
+    struct tree *too_root;
+
+    /* Allocate memory space for 9 nodes */
+    struct _too *too;
+    too = malloc(sizeof(struct _too) * ELE_NUM);
+
+    /* Initialize 9 nodes */
+    for (i = 0; i < 9; i++) {
+        INIT_TREE(&too[i].tree);
+        too[i].a = 'A' + i;
+    }
+
+    // debug
+    for (i = 0; i < 9; i++) {
+        printf("%c ", tree_entry(&too[i].tree, struct _too, tree)->a);
+    }
+    printf("\n");
+
+    too_root = &too[0].tree;
+
+    /* Create tree and subtree and connect them */
+    // 0A 1B 2C 3D 4E 5F 6G 7H 8J
+    // A-B
+    tree_add_l(&too[1].tree, &too[0].tree);
+    // A-C
+    tree_add_r(&too[2].tree, &too[0].tree);
+
+    // // debug
+    // for (i = 0; i < 9; i++) {
+    //     printf("%c ", tree_entry(&too[i].tree, struct _too, tree)->a);
+    // }
+    // printf("\n");
+
+    // printf("%c %c %c\n", 
+    //     tree_entry(&too[0].tree, struct _too, tree)->a, 
+    //     tree_entry(too[0].tree.llink, struct _too, tree)->a,
+    //     tree_entry(too[0].tree.rlink, struct _too, tree)->a);
+
+    // B-D
+    tree_add_l(&too[3].tree, &too[1].tree);
+
+    // C-E
+    tree_add_l(&too[4].tree, &too[2].tree);
+    // C-F
+    tree_add_r(&too[5].tree, &too[2].tree);
+
+    // printf("%c %c %c\n", 
+    //     tree_entry(&too[2].tree, struct _too, tree)->a, 
+    //     tree_entry(too[2].tree.llink, struct _too, tree)->a,
+    //     tree_entry(too[2].tree.rlink, struct _too, tree)->a);
+
+    // E-G
+    tree_add_r(&too[6].tree, &too[4].tree);
+
+    // F-H
+    tree_add_l(&too[7].tree, &too[5].tree);
+    // F-J
+    tree_add_r(&too[8].tree, &too[5].tree);
+
+    tree_inorder_traverse_test(too_root);
+
+    free(too);
+
+    return;
 }
--- tree.h
+++ tree.h
@@ -3,10 +3,55 @@
 
 #include "types.h"
 #include "menu.h"
+#include "list.h"
 
 #include <stdio.h>
 
-typedef struct list_head tree_head;
+// #define tree_add_l  list_add
+// #define tree_add_r  list_add_tail
+// #define TREE   LIST_HEAD
+// #define list tree
+
+// typedef struct list_head TREE;
+
+#define CONFIG_TREE_DEBUG_MESSAGE       (TRUE)
+#if (CONFIG_TREE_DEBUG_MESSAGE)
+    #define TREE_ASSERT (cond)      ASSERT(cond)
+#else
+    #define TREE_ASSERT(cond)
+#endif
+
+struct tree {
+    struct tree *llink, *rlink;
+};
+
+#define TREE_INIT(name)    {&(name), &(name)}
+
+#define TREE(name) \
+    struct tree name = TREE_INIT(name)
+
+#define tree_entry(ptr, type, member) \
+    ((type *)( (char *)(ptr) - ((size_t)&((type *)0)->member) ))
+
+static inline void INIT_TREE(struct tree *tree)
+{
+    tree->llink = NULL;
+    tree->rlink = NULL;
+}
+
+static inline void tree_add_l(
+    struct tree *child,
+    struct tree *parent)
+{
+    parent->llink = child;
+}
+
+static inline void tree_add_r(
+    struct tree *child,
+    struct tree *parent)
+{
+    parent->rlink = child;
+}
 
 ret_code menu_func_tree(int argc, char **argv);
 
